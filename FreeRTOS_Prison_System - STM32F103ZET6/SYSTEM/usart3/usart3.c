006a74fa57cdeec00d87b31a8f9f6e3439509f56
modecreate <59120014+modecreate@users.noreply.github.com>
2024-02-20 22:54:15 +0800

    Add files via upload

---
+++ FreeRTOS_Prison_System - STM32F103ZET6/SYSTEM/usart3/usart3.c
@@ -0,0 +1,151 @@
+#include "sys.h"
+#include "usart3.h"	  
+#include "stdarg.h"
+////////////////////////////////////////////////////////////////////////////////// 	 
+//如果使用ucos,则包括下面的头文件即可.
+#if SYSTEM_SUPPORT_OS
+#include "FreeRTOS.h"					//ucos 使用	  
+#endif
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK STM32开发板
+//串口1初始化		   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2012/8/18
+//版本：V1.5
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
+//All rights reserved
+//********************************************************************************
+//V1.3修改说明 
+//支持适应不同频率下的串口波特率设置.
+//加入了对printf的支持
+//增加了串口接收命令功能.
+//修正了printf第一个字符丢失的bug
+//V1.4修改说明
+//1,修改串口初始化IO的bug
+//2,修改了USART_RX_STA,使得串口最大接收字节数为2的14次方
+//3,增加了USART_REC_LEN,用于定义串口最大允许接收的字节数(不大于2的14次方)
+//4,修改了EN_USART1_RX的使能方式
+//V1.5修改说明
+//1,增加了对UCOSII的支持
+////////////////////////////////////////////////////////////////////////////////// 	  
+ 
+
+//////////////////////////////////////////////////////////////////
+
+ 
+ 
+#if EN_USART3_RX   //如果使能了接收
+//串口1中断服务程序
+//注意,读取USARTx->SR能避免莫名其妙的错误   	
+u8 USART3_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
+u8 USART3_TX_BUF[USART3_MAX_SEND_LEN]; 			  //发送缓冲,最大USART3_MAX_SEND_LEN字节
+//接收状态
+//bit15，	接收完成标志
+//bit14，	接收到0x0d
+//bit13~0，	接收到的有效字节数目
+u16 USART3_RX_STA=0;       //接收状态标记	  
+u32 count=0;  
+void uart3_init(u32 bound){
+	//GPIO端口设置
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);	//使能GPIOB时钟
+	//RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);	//使能AFIO复用时钟
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);	//使能USART3时钟
+	
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10; //PB10_USART3_TX
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	//复用推挽输出
+	GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIOB.10
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;//PB11_USART3_RX
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//浮空输入
+	GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIOB.11
+
+
+	//USART 初始化设置
+	USART_DeInit(USART3); //复位串口1
+	USART_InitStructure.USART_BaudRate = bound;//串口波特率
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;//无奇偶校验位
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
+  USART_Init(USART3, &USART_InitStructure); //初始化串口1	
+
+	//Usart1 NVIC 配置
+	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;//串口1中断
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;//抢占优先级3
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//子优先级3
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
+	NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器
+
+  USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);//开启串口接受中断
+  USART_ITConfig(USART3, USART_IT_IDLE, ENABLE);//开启串口接受中断
+  USART_Cmd(USART3, ENABLE);                    //使能串口1 
+}
+
+
+//串口3,printf 函数
+//确保一次发送数据不超过USART2_MAX_SEND_LEN字节
+void u3_printf(char* fmt,...)  
+{  
+	u16 i,j; 
+	va_list ap; 
+	va_start(ap,fmt);
+	vsprintf((char*)USART3_TX_BUF,fmt,ap);
+	va_end(ap);
+	i=strlen((const char*)USART3_TX_BUF);		//此次发送数据的长度
+	for(j=0;j<i;j++)							//循环发送数据
+	{
+	  while(USART_GetFlagStatus(USART2,USART_FLAG_TC)==RESET); //循环发送,直到发送完毕   
+		USART_SendData(USART2,USART3_TX_BUF[j]); 
+	} 
+}
+
+void USART3_IRQHandler(void)                	//串口3中断服务程序
+	{
+	u8 Res;
+		USART3_RX_STA=0;
+#if SYSTEM_SUPPORT_OS 		//如果SYSTEM_SUPPORT_OS为真，则需要支持OS.
+   
+#endif
+if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
+		{
+		Res =USART_ReceiveData(USART3);	//读取接收到的数据
+		USART3_RX_BUF[count++]=Res;
+		
+//		if((USART3_RX_STA&0x8000)==0)//接收未完成
+//			{
+//			if(USART3_RX_STA&0x4000)//接收到了0x0d
+//				{
+//				if(Res!=0x0a)USART3_RX_STA=0;//接收错误,重新开始
+//				else USART3_RX_STA|=0x8000;	//接收完成了 
+//				}
+//			else //还没收到0X0D
+//				{	
+//				if(Res==0x0d)USART3_RX_STA|=0x4000;
+//				else
+//					{
+//					USART3_RX_BUF[USART3_RX_STA&0X3FFF]=Res ;
+//					USART3_RX_STA++;
+//					if(USART3_RX_STA>(USART_REC_LEN-1))USART3_RX_STA=0;//接收数据错误,重新开始接收	  
+//					}		 
+//				}
+//			}   		 
+     }
+if(USART_GetITStatus(USART3, USART_IT_IDLE) != RESET)	{
+	USART3->SR;
+  USART3->DR;
+  USART3_RX_STA=1;
+}
+#if SYSTEM_SUPPORT_OS 	//如果SYSTEM_SUPPORT_OS为真，则需要支持OS.									
+		 
+#endif
+} 
+#endif	
+
